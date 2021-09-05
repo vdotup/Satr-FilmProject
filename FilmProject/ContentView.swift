2f0c121be019a036d5a1e63c5a24e8fcd27d2514
vdotup <v.up@live.com>
2021-09-05 12:17:54 +0300

    Initial Commit

---
+++ FilmProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FilmProject
+//
+//  Created by Abdurrahman Alfudeghi on 05/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FilmProject/FilmProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmProjectApp.swift
+//  FilmProject
+//
+//  Created by Abdurrahman Alfudeghi on 05/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct FilmProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
